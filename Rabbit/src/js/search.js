8afd603fcf99b9f5dd2702e3810c9147d3fa67dd
Kantemir Kuzminov <kantik1406@gmail.com>
2022-12-09 16:04:55 +0300

    Revert "Rabbit 1.0"
    This reverts commit 2a5d27bb9d37d0a436b04d3b1ba055fad5264370.

--- Rabbit/src/js/search.js
+++
@@ -1,6 +0,0 @@
-const search = document.querySelector(".search"),
-    searchInput = search.querySelector(".search__input");
-
-search.addEventListener("touchstart", () => {
-    searchInput.style.display="none";
-});
